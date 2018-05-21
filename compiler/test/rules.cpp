2eb22dbe0827a822923688befcc2df205f81df54
byxor <brandnewbyxor@gmail.com>
2018-05-21 22:44:51 +0100

    Empty answers are non-compliant

--- compiler/test/rules.cpp
+++ compiler/test/rules.cpp
@@ -2,19 +2,19 @@
 #include "question.hpp"
 #include "catch.hpp"
 
+const Question unanswered[] = {
+  Question("Is this real life?"),
+  Question("Are carrots orange?"),
+  Question("How far can one push their productivity?"),
+};
+
+const Question answered[] = {
+  Question("How many cars can you see?", "0"),
+  Question("How old are you?", "20"),
+  Question("Is bitcoin going to crash?", "Probably"),
+};
+
 TEST_CASE("Questions") {
-
-  const Question unanswered[] = {
-    Question("Is this real life?"),
-    Question("Are carrots orange?"),
-    Question("How far can one push their productivity?"),
-  };
-
-  const Question answered[] = {
-    Question("How many cars can you see?", "0"),
-    Question("How old are you?", "20"),
-    Question("Is bitcoin going to crash?", "Probably"),
-  };
 
   SECTION("are invalid without answers") {
     for (auto const &question: unanswered) {
