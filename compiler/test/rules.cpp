92a7cfc746cdb519f114c4a898e6d3fd2ccad631
byxor <brandnewbyxor@gmail.com>
2018-05-21 23:16:22 +0100

    Refactor

--- compiler/test/rules.cpp
+++ compiler/test/rules.cpp
@@ -2,17 +2,16 @@
 #include "thought.hpp"
 #include "catch.hpp"
 
-const Answer valid[] = {
-  Answer("Yes"),
-  Answer("No"),
-  Answer("250"),
-};
-
 TEST_CASE("Answers") {
   SECTION("are invalid when empty") {
     REQUIRE_THROWS_AS(Answer("").AssertCompliant(), NotAnAnswer);
   }
   SECTION("are valid otherwise") {
+    const Answer valid[] = {
+      Answer("Yes"),
+      Answer("No"),
+      Answer("250"),
+    };
     for (auto const &answer: valid)
       answer.AssertCompliant();
   }
